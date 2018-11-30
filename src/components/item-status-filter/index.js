a89df6592a401778ebe280517b591b4551db3606
Petr Komlev <petr.komlev@gmail.com>
2018-11-30 16:50:17 +0300

    Add item-status-filter component

---
+++ src/components/item-status-filter/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ItemStatusFilter extends React.Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-outline-secondary active">All</button>
+        <button type="button" className="btn btn-outline-secondary">Active</button>
+        <button type="button" className="btn btn-outline-secondary">Done</button>
+      </div>
+    );
+  }
+}
