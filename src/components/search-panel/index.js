4280eea35d33aa1dac64f831086ea7606939d519
Petr Komlev <petr.komlev@gmail.com>
2019-02-07 17:06:36 +0300

    add searchTodo function to property

--- src/components/search-panel/index.js
+++ src/components/search-panel/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ItemStatusFilter from '../item-status-filter/';
 
-const SearchPanel = () => {
+const SearchPanel = ({ searchTodo }) => {
   return (
     <div className="d-flex mb-2">
       <div className="input-group mr-2">
-        <input type="text" className="form-control" placeholder="Search" />
+        <input type="text" 
+               className="form-control" 
+               placeholder="Search" 
+               onChange={(event) => searchTodo(event)} />
       </div>
       <ItemStatusFilter />
     </div>
